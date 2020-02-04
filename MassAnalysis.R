b0be884de0d8e603bbfd459430b50b85c08603b8
Hugh Shanahan <Hugh.Shanahan@rhul.ac.uk>
2020-02-04 07:52:46 +0000

    Updated script so that it is called with one line, more data

--- MassAnalysis.R
+++ MassAnalysis.R
@@ -7,21 +7,23 @@ daysSinceStart <- function(){
   as.numeric(ISOdate(year,month,day)- ISOdate("2020","2","1"),units='days')
 }
 
+runThis(){
 # Read in masses data frame then ask for mass for today
-massesFn <- "masses.Rdata"
-load(file=massesFn)
-m <- as.numeric(readline("What is your mass today?"))
+  massesFn <- "masses.Rdata"
+  load(file=massesFn)
+  m <- as.numeric(readline("What is your mass today?"))
 
 # Update masses data frame and store
-thisDay <- data.frame(Today=c(Sys.Date()),daysElapsed=c(daysSinceStart()),mass=c(m*1.0))
-masses <- rbind(masses,thisDay)
-save(masses,file=massesFn)
+  thisDay <- data.frame(Today=c(Sys.Date()),daysElapsed=c(daysSinceStart()),mass=c(m*1.0))
+  masses <- rbind(masses,thisDay)
+  save(masses,file=massesFn)
 
 # Plot all the data
-library(ggplot2)
-ggplot(data=masses,aes(daysElapsed,mass)) + geom_point() + scale_x_continuous(limits = c(0,30)) + scale_y_continuous(limits=c(75,90)) + stat_smooth(method="loess",fullrange=TRUE)
+  library(ggplot2)
+  ggplot(data=masses,aes(daysElapsed,mass)) + geom_point() + scale_x_continuous(limits = c(0,30)) + scale_y_continuous(limits=c(75,90)) + stat_smooth(method="loess",fullrange=TRUE)
+}
 
-
+runThis()
 
 
 
